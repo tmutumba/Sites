65957ce6ea0e83fcb3934c674429b98067aef57a
tmutumba <mutumbatonny@gmail.com>
2021-03-15 15:22:30 -0600

    form errors helpers added

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,8 @@ class Subject < ApplicationRecord
 
 	has_many :pages
 
+	validates_presence_of :name
+
 	scope :visible, lambda { where(:visible => true) }
 	scope :invisible, lambda { where(:visible => false) }
 	scope :sorted, lambda { order("subjects.position ASC") }
